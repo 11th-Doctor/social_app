18e409f11f54342dc06fa48f5b5196055ccc0489
Daryl Zhang <august9382@gmail.com>
2021-08-23 22:22:36 +0800

    feat: Added router Comment

--- routes/comment.js
+++ routes/comment.js
@@ -4,6 +4,21 @@ moment.locale('zh-TW')
 var router = express.Router()
 const Comment = require('../models/Comment')
 
+router.get('/:postId', async (req, res) => {
+    const postId = req.params.postId
+
+    var comments = await Comment.find({post: postId})
+    .populate('user', '_id fullName profileImageUrl emailAddress')
+    .lean()
+    .exec()
+
+    comments.forEach(comment => {
+        comment.fromNow = moment(comment.createdAt, 'YYYYMMDD').fromNow()
+    })
+    
+    res.json(comments)
+})
+
 router.post('/:postId', async (req, res) => {
     const userId = req.session.userId
     const postId = req.params.postId
